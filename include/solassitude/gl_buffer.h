fc2a96fb07d05f02af1c645746958bd4605e0ca0
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2022-04-04 20:12:09 +0300

    Say goodbye to old implementation of gl::buffer. We will rewrite it afterwards!

--- include/solassitude/gl_buffer.h
+++ include/solassitude/gl_buffer.h
@@ -43,57 +43,5 @@ namespace solassitude {
 		private:
 			buffer_target m_target;
 		};
-		
-		/*
-		
-		template <buffer_target target_p = buffer_target::array> 
-		class buffer : public object {
-		private:
-			friend class buffers;
-			explicit buffer(unsigned int i) : object{ i } { }
-			
-			static void bind_nontemplated(buffer_target, unsigned int);
-		public:
-			buffer<target_p> &bind(void) {
-				bind_nontemplated(target_p, id());
-				return *this;
-			}
-			
-			constexpr buffer_target target(void) const noexcept {
-				return target_p;
-			}
-		};
-		
-		class buffer {
-		private:
-			friend class buffers;
-			explicit buffer(unsigned int id) : m_id{ id }, m_target{ buffer_target::none } { }
-			
-		public:
-			buffer &bind(buffer_target target = buffer_target::array);
-			
-		private:
-			unsigned int m_id;
-			buffer_target m_target;
-		};
-		
-		class buffers {
-		public:
-			explicit buffers(int n = 1);
-			~buffers();
-			
-			buffer operator[](int i) noexcept {
-				return buffer{ m_ids.at(i) };
-			}
-			
-			unsigned int size(void) const noexcept {
-				return m_ids.size();
-			}
-			
-		private:
-			std::vector<unsigned int> m_ids;
-		};
-		
-		*/
 	}
 }
