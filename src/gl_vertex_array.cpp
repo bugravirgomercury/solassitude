e145d308a9afbf8bcc16940ea3b96c6d9ccd7492
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2022-04-07 20:43:38 +0300

    Remove dead code from gl::vertex_array

--- src/gl_vertex_array.cpp
+++ src/gl_vertex_array.cpp
@@ -12,29 +12,6 @@
 
 #include <stdexcept> // std::logic_error
 
-/*
-solassitude::gl::vertex_arrays::vertex_arrays(int n) {
-	m_vaos.resize(n);
-	glGenVertexArrays(n, m_vaos.data());
-	
-	__GL_CHECK_ERROR
-}
-
-solassitude::gl::vertex_arrays::~vertex_arrays() {
-	glDeleteVertexArrays(m_vaos.size(), m_vaos.data());
-	
-	// No need to check for error, n will be always greater than 0.
-}
-
-solassitude::gl::vertex_array &solassitude::gl::vertex_array::bind(void) {
-	glBindVertexArray(id());
-	
-	__GL_CHECK_ERROR
-	
-	return *this;
-}
-*/
-
 static inline GLuint _solassitude_new_vertex_array() {
 	GLuint object_id;
 	::glGenVertexArrays(1, &object_id);
