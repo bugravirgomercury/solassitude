d8840b1978c6015e2d24392f89adc60c0ba8883d
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2022-04-07 20:13:10 +0300

    Reimplemented gl::vertex_array in terms of gl::reference

--- include/solassitude/gl_vertex_array.h
+++ include/solassitude/gl_vertex_array.h
@@ -1,49 +1,31 @@
 #pragma once
 
 #include "gl_abstract.h"
-#include <vector>
+#include "gl_reference.h"
+//#include <vector>
+#include <utility> // std::forward
 
 namespace solassitude {
 	namespace gl {
-		class vertex_arrays;
 		class vertex_array;
-		/*
-		class vertex_array {
-		private:
-			friend class vertex_arrays;
-			explicit vertex_array(unsigned int vao) : m_vao{ vao } { }
-			
-		public:
-			vertex_array &bind(void);
-			
-		private:
-			unsigned int m_vao;	
-		};*/
 		
-		class vertex_array : public object {
-		private:
-			friend class vertex_arrays;
-			explicit vertex_array(unsigned int vao) : object{ vao } { }
+		class vertex_array : 
+			public reference<vertex_array> {
+				
 		public:
-			vertex_array &bind(void);
-			~vertex_array() { }
-		};
-		
-		class vertex_arrays {
-		public:
-			explicit vertex_arrays(int n = 1);
-			~vertex_arrays();
+			explicit vertex_array(unsigned int id) : reference{ id } { }
+			explicit vertex_array(); // generate one with a new id.
+			~vertex_array() { } // destructor needs to be defined, compiler will implicitly call reference::~reference()
+			explicit vertex_array(const vertex_array &copy) : reference{ copy } { } // c++ does not inherit constructors
+			explicit vertex_array(vertex_array &&move) : reference{ std::move(move) } { }
 			
-			vertex_array operator[](int i) noexcept {
-				return vertex_array{ m_vaos.at(i) }.bind();
-			}
+		protected:
+			// The base class will call on_* methods (CRTP/NVI pattern).
 			
-			unsigned int size() const noexcept {
-				return m_vaos.size();
-			}
-			
-		private:
-			std::vector<unsigned int> m_vaos;
+			void on_destruct(void); // called by the base destructor
+			void on_activate(void); // called by base activate() method
+			void on_copy(const vertex_array &copy, bool is_assign); // called by base copy c'tor and assignment operator
+			void on_move(vertex_array &&move, bool is_assign); // called by base move c'tor and assignment operator
 		};
 	}
 }
--- src/gl_vertex_array.cpp
+++ src/gl_vertex_array.cpp
@@ -10,6 +10,9 @@
 
 #define __GL_CHECK_ERROR GLenum err{ ::glGetError() }; if (err != GL_NO_ERROR) { throw solassitude::gl::error{ err }; }
 
+#include <stdexcept> // std::logic_error
+
+/*
 solassitude::gl::vertex_arrays::vertex_arrays(int n) {
 	m_vaos.resize(n);
 	glGenVertexArrays(n, m_vaos.data());
@@ -30,3 +33,45 @@ solassitude::gl::vertex_array &solassitude::gl::vertex_array::bind(void) {
 	
 	return *this;
 }
+*/
+
+static inline GLuint _solassitude_new_vertex_array() {
+	GLuint object_id;
+	::glGenVertexArrays(1, &object_id);
+	__GL_CHECK_ERROR
+	return object_id;
+}
+
+solassitude::gl::vertex_array::vertex_array() : vertex_array{ _solassitude_new_vertex_array() } 
+{
+}
+
+void solassitude::gl::vertex_array::on_destruct(void) {
+	// Called by d'tor - no throwing exceptions, otherwise std::terminate is called
+	// This method won't be called if c'tor threw exception, as that means the object
+	// is NOT constructed, no d'tor will be called. Object ID is guaranteed to be valid.
+	GLuint object_id{ id() }; // don't use pointer to prvalue as an input argument for pointer parameter!
+	::glDeleteVertexArrays(1, &object_id);
+	__GL_CHECK_ERROR
+}
+
+void solassitude::gl::vertex_array::on_activate(void) {
+	::glBindVertexArray(id());
+	__GL_CHECK_ERROR
+}
+
+void solassitude::gl::vertex_array::on_copy(const solassitude::gl::vertex_array &copy, bool is_assign) {
+	// TODO: How to clone a VAO? Help wanted.
+	throw std::logic_error{ "VAO cloning not yet implemented" };
+}
+
+void solassitude::gl::vertex_array::on_move(solassitude::gl::vertex_array &&move, bool is_assign) {
+	// Make use the VAO is detached
+	::glBindVertexArray(0);
+	
+	if (is_assign)
+		this->on_destruct(); // Move assignment, destroy current one
+		
+	// ID move is handled by the base class, and we have no more fields to copy.
+}
+
