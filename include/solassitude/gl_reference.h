3e034a325ceeb93bfe12114de1867c81bc43ebe3
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2022-04-07 19:13:33 +0300

    Made gl::reference header more spacious

--- include/solassitude/gl_reference.h
+++ include/solassitude/gl_reference.h
@@ -8,15 +8,21 @@ namespace solassitude {
 		template <class derived_t> class reference {
 		public:
 			explicit reference(unsigned int _id) : m_id{ _id } { }
+			
 			~reference() { static_cast<derived_t &>(*this).on_destruct(); }
+			
 			constexpr unsigned int id(void) const noexcept { return m_id; }
+			
 			constexpr void id(unsigned int id) noexcept { m_id = id; }
+			
 			explicit reference(const reference<derived_t> &copy) : m_id{ copy.m_id } { 
 				static_cast<derived_t *>(this)->on_copy(static_cast<const derived_t &>(std::forward(copy)), false);
 			}
+			
 			explicit reference(reference<derived_t> &&move) : m_id{ std::move(move.m_id) } {
 				static_cast<derived_t *>(this)->on_move(static_cast<derived_t &&>(std::forward(move)), false);
 			}
+			
 			reference<derived_t> &operator =(const reference<derived_t> &copy) {
 				if (&copy != this) {
 					m_id = copy.m_id;
@@ -24,6 +30,7 @@ namespace solassitude {
 				}
 				return *this;
 			}
+			
 			reference<derived_t> &operator =(reference<derived_t> &&move) {
 				if (&move != this) {
 					m_id = std::move(move.m_id);
@@ -31,6 +38,7 @@ namespace solassitude {
 				}
 				return *this;
 			}
+			
 			void activate(void) { static_cast<derived_t &>(*this).on_activate(); }
 			
 		protected:
